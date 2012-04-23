785a6730c8de976b325262a15452b19ce7e45b07
Ryan Clark <ryan.clark.j@gmail.com>
2012-04-23 15:15:55 -0700

    Add ID to admin page

--- admin.py
+++ admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from models import EarthquakeData
 
 class EQAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'magnitude', 'type', 'depth', 'occurance_date', 'source_catalog', 'latitude', 'longitude')
+    list_display = ('id', '__unicode__', 'magnitude', 'type', 'depth', 'occurance_date', 'source_catalog', 'latitude', 'longitude')
+    list_display_links = ('__unicode__',)
     exclude = ('geom',)
     
 admin.site.register(EarthquakeData, EQAdmin)
