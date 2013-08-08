5e55529f8526573d32b64bddf987c51cc91021f3
Ryan Clark <ryan.clark.j@gmail.com>
2013-08-08 15:15:58 -0700

    reorder list_display fields

--- admin.py
+++ admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from models import EarthquakeData
 
 class EQAdmin(admin.ModelAdmin):
-    list_display = ('id', '__unicode__', 'magnitude',  'type', 'rms', 'depth', 'erz', 'occurance_date', 'source_catalog', 'latitude', 'longitude', 'erh')
+    list_display = ('id', '__unicode__', 'magnitude',  'type', 'rms', 'depth', 'erz', 'erh', 'occurance_date', 'source_catalog', 'latitude', 'longitude')
     list_display_links = ('__unicode__',)
     exclude = ('geom', 'calculated_magnitude')
     
