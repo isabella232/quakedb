a6d647b001cd2cbcae1c445ff121cd45b9b37fec
asonnenschein <adrian.sonnenschein@gmail.com>
2014-05-01 22:59:02 -0700

    style eqs

--- static/quakedb/map/scripts/app.js
+++ static/quakedb/map/scripts/app.js
@@ -27,15 +27,36 @@ data = [
   	serviceType: "WFS",
   	active: true,
   	layerOptions: {
-	  pointToLayer: function (feature, latlng) {
-	    markerOptions = {
-	  	  radius: 10,
-	  	  fillColor: "red",
-	  	  color: "orange",
-	  	};
-	    return L.circleMarker(latlng, markerOptions);
-	  },
-	}
+	    pointToLayer: function (feature, latlng) {
+        var color;
+	      markerOptions = {
+          fillOpacity: 0.8,
+          stroke: 0,
+        }
+        var mag = feature.properties.calculated_magnitude;
+        if (0 < mag && mag <= 1) {
+          color = "rgb(255,255,204)";
+        } else if (1 < mag && mag <= 2) {
+          color = "rgb(255,237,160)";
+        } else if (2 < mag && mag <= 3) {
+          color = "rgb(254,217,118)";
+        } else if (3 < mag && mag <= 4) {
+          color = "rgb(254,178,76)";
+        } else if (4 < mag && mag <= 5) {
+          color = "rgb(253,141,60)";
+        } else if (5 < mag && mag <= 6) {
+          color = "rgb(252,78,42)";
+        } else if (6 < mag && mag <= 7) {
+          color = "rgb(227,26,28)";
+        } else if (7 < mag && mag <= 8) {
+          color = "rgb(177,0,38)";
+        }
+
+        markerOptions.radius = mag * 5;
+        markerOptions.color = markerOptions.fillColor = color;
+	      return L.circleMarker(latlng, markerOptions);
+	    },
+	  }
   }),
   new app.models.GeoJSONLayer({
   	id: "activefaults",
